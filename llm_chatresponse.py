f0fe1df053a36c7efbc07272339850cf51f21eeb
Natasha Gaitonde <n.gaitonde@gmail.com>
2025-05-18 17:18:15 -0700

    Initial hello world for chat with a LM

--- llm_chatresponse.py
+++ llm_chatresponse.py
@@ -5,18 +5,6 @@ import os
 
 load_dotenv()
 
-class SimpleQA(dspy.Signature):
-    question = dspy.InputField()
-    answer = dspy.OutputField()
-
-class SimpleAnswerer(dspy.Module):
-    def __init__(self):
-        super().__init__()
-        self.predictor = dspy.Predict(SimpleQA)
-
-    def forward(self, question):
-        return self.predictor(question=question)
-
 if __name__=="__main__":
 
     api_key = os.environ.get("OPENAI_API_KEY")
@@ -27,13 +15,5 @@ if __name__=="__main__":
     lm = dspy.LM('openai/gpt-4o', api_key=api_key)
     dspy.configure(lm=lm)
 
-    # chat_response = lm("Say this is a test!", temperature=0.7)
-    # chat_response = lm(messages=[{"role": "user", "content": "Say this is a test!"}])
-    # print(f"Chat response: {chat_response}")
-
-    answerer = SimpleAnswerer()
-    question = "What is the capital of India?"
-    chat_response = answerer(question)
-    
-    print(f"Question: {question}")
-    print(f"Answer: {chat_response.answer}")   
+    chat_response = lm(messages=[{"role": "user", "content": "Say Hello World!"}])
+    print(f"Chat response: {chat_response}")
